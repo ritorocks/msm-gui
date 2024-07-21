34d20b404cae4a0071b7fea2b9fb362dd3bcd38b
ritorocks <ritorocks@gmail.com>
2024-07-21 22:48:16 +0000

    adds actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -12,6 +12,43 @@ class ActorsController < ApplicationController
     redirect_to("/actors")
   end
   
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => the_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
+  def update
+  # Get the ID out of params
+
+  a_id = params.fetch("the_id")
+  # Look up the exisiting record
+
+  matching_records = Actor.where({ :id => a_id })
+  the_actor = matching_records.at(0)
+
+  # Overwrite each column with the values from user inputs
+
+   the_actor.name = params.fetch("query_name")
+   the_actor.dob = params.fetch("query_dob")
+   the_actor.bio = params.fetch("query_bio")
+   the_actor.image = params.fetch("query_image")
+   
+
+  # Save
+   the_actor.save
+  # Redirect to the movie details page
+
+  redirect_to("/actors/#{the_actor.id}")
+
+
+  end
   
   def index
     matching_actors = Actor.all
